ff4b8816d7fa923c546d1df1d13af64eba79a9a0
jayc971 <contact@jayanika.com>
2025-02-06 10:21:27 +0530

    dsads

--- src/app/components/controls/page.tsx
+++ src/app/components/controls/page.tsx
@@ -1,4 +1,6 @@
 'use client'
+
+import React from 'react';
 import { Activity, Move, Trash2, X } from 'lucide-react';
 
 interface ModeControlsProps {
@@ -9,35 +11,44 @@ interface ModeControlsProps {
   onClearSelection: () => void;
 }
 
-const Controls: React.FC<ModeControlsProps> = ({
+const Controls = ({
   connectMode,
   adjustMode,
   deleteMode,
   onToggleMode,
   onClearSelection,
-}: ModeControlsProps) => {
+}: ModeControlsProps): JSX.Element => {
   return (
     <div className="flex gap-4 mb-4">
       <button
-        className={`p-2 hover:bg-gray-500 rounded ${connectMode ? 'bg-blue-500 text-white' : 'bg-gray-800 text-white'}`}
+        className={`p-2 hover:bg-gray-500 rounded ${
+          connectMode ? 'bg-blue-500 text-white' : 'bg-gray-800 text-white'
+        }`}
         onClick={() => onToggleMode('connect')}
       >
         <Activity size={24} />
       </button>
       <button
-        className={`p-2 hover:bg-gray-500 rounded ${adjustMode ? 'bg-green-500 text-white' : 'bg-gray-800 text-white'}`}
+        className={`p-2 hover:bg-gray-500 rounded ${
+          adjustMode ? 'bg-green-500 text-white' : 'bg-gray-800 text-white'
+        }`}
         onClick={() => onToggleMode('adjust')}
       >
         <Move size={24} />
       </button>
       <button
-        className={`p-2 hover:bg-gray-500 rounded ${deleteMode ? 'bg-red-500 text-white' : 'bg-gray-800 text-white'}`}
+        className={`p-2 hover:bg-gray-500 rounded ${
+          deleteMode ? 'bg-red-500 text-white' : 'bg-gray-800 text-white'
+        }`}
         onClick={() => onToggleMode('delete')}
       >
         <Trash2 size={24} />
       </button>
       {connectMode && (
-        <button className="p-2 hover:bg-red-500 rounded bg-red-600 text-white" onClick={onClearSelection}>
+        <button 
+          className="p-2 hover:bg-red-500 rounded bg-red-600 text-white" 
+          onClick={onClearSelection}
+        >
           <X size={24} />
         </button>
       )}
